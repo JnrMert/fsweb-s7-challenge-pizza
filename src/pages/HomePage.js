e5077d8e77028bead7e6b547b3ae1dbf1df6c7f6
Mert Yazıcı | Newer! <yzc.mert@icloud.com>
2024-08-12 11:18:33 +0300

    Anasayfa

---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomePage = (props) => {
+  return (
+    <div className="home-page">
+      <div className="home-page-content">
+        <h1>Teknolojik Yemekler</h1>
+        <p>
+          KOD ACIKTIRIR <br></br> PIZZA, DOYURUR
+        </p>
+        <Button />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
