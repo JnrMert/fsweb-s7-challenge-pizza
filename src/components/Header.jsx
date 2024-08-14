cdcd803ddfdb88d7761ccdb7d16ed48733910ea3
Mert Yazıcı | Newer! <yzc.mert@icloud.com>
2024-08-14 14:51:16 +0300

    Header.jsx Header.css

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Link } from "react-router-dom";
+
+const Header = (props) => {
+  return (
+    <>
+      <div className="header-main"></div>
+      <div className="header">
+        <div className="header-title">
+          <h1>Teknolojik Yemekler</h1>
+        </div>
+        <div className="header-nav">
+          <Link to="/">
+            <p>
+              Anasayfa - Seçenekler -{" "}
+              <span style={{ fontWeight: "700" }}>Sipariş Oluştur</span>
+            </p>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Header;
