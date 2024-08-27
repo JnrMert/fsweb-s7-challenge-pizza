75a700af14f2d03fb11ded7193755626fa268391
Mert Yazıcı | Newer! <yzc.mert@icloud.com>
2024-08-28 00:32:48 +0300

    Pizza v0.1

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,32 +1,47 @@
 import React from "react";
-import {useState} from 'react';
+import { useState } from "react";
 import * as Yup from "yup";
-import orderSchema from "../validations/OrderSchema";
+import orderSchema from "../validations/OrderSchema.jsx";
 /* 
 
 
 */
-const Note = ({orderNote}) => {
-    const [note, setNote] = useState('');
-    orderNote(note);
-    const [formState,setFormState] = useState({isim:"" });
-    const [formErrors,setformErrors] = useState({isim:""})
-    const inputChange= e=>{
-        setNote(e.target.value)
-        const {name, value}=e.target;
-        Yup.reach(orderSchema,name)
-            .validate(value)
-            .then(valid=>{setformErrors({...formErrors,[name]:""});})
-            .catch(err=>{setformErrors({...formErrors,[name]:err.errors[0]});})
-        setFormState({...formState,[name]:value}) 
-    }
-    return (
-        <div className="ordernote">
-            <h4>Sipariş Notu</h4>
-            <input name="name" type="text" id="name-input" placeholder="Lütfen isminizi giriniz." onChange={(e)=>inputChange(e)}></input>
-            <input type="text" name="note" id="special-text" placeholder="Siparişine eklemek istediğin bir not var mı?"></input>
-            <hr></hr>
-        </div>
-    )
+const Note = ({ orderNote }) => {
+  const [note, setNote] = useState("");
+  orderNote(note);
+  const [formState, setFormState] = useState({ isim: "" });
+  const [formErrors, setformErrors] = useState({ isim: "" });
+  const inputChange = (e) => {
+    setNote(e.target.value);
+    const { name, value } = e.target;
+    Yup.reach(orderSchema, name)
+      .validate(value)
+      .then((valid) => {
+        setformErrors({ ...formErrors, [name]: "" });
+      })
+      .catch((err) => {
+        setformErrors({ ...formErrors, [name]: err.errors[0] });
+      });
+    setFormState({ ...formState, [name]: value });
+  };
+  return (
+    <div className="ordernote">
+      <h4>Sipariş Notu</h4>
+      <input
+        name="name"
+        type="text"
+        id="name-input"
+        placeholder="Lütfen isminizi giriniz."
+        onChange={(e) => inputChange(e)}
+      ></input>
+      <input
+        type="text"
+        name="note"
+        id="special-text"
+        placeholder="Siparişine eklemek istediğin bir not var mı?"
+      ></input>
+      <hr></hr>
+    </div>
+  );
 };
 export default Note;
